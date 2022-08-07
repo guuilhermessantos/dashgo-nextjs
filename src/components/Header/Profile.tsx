64b5d555a0c34ff785731701a0eb100d1fc31071
Guilherme Santos <guuilhermessantos@gmail.com>
2022-08-07 19:54:22 -0300

    finalizando responsividade

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean
+}
+
+export function Profile({ showProfileData =  true }: ProfileProps) {
   return (
     <Flex align="center" >
-      <Box mr="4" textAlign="right">
-        <Text> Guilherme Santos </Text>
-        <Text color="gray.300" fontSize="small" > 
-          guuilhermessantos@gmail.com 
-        </Text>
-      </Box>
+      { showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text> Guilherme Santos </Text>
+          <Text color="gray.300" fontSize="small" > 
+            guuilhermessantos@gmail.com 
+          </Text>
+        </Box>
+      )}
 
       <Avatar size="md" name="Guilherme Santos" src="https://github.com/guuilhermessantos.png"/>
       </Flex>
